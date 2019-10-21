1fc0b26743b6db34e4780c46352265ff6622f41b
daniel <daniel.jarvis@mail.utoronto.ca>
2019-10-21 10:47:23 -0400

    Changed activity selection to use a spinner and a single transition button.

--- phase1/app/src/main/java/com/example/postmortem/MainActivity.java
+++ phase1/app/src/main/java/com/example/postmortem/MainActivity.java
@@ -5,19 +5,62 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+  /** The spinner menu items */
+  public final static String MENU_ACTIVITY = "menu activity";
+
+  private final String[] activities = {MENU_ACTIVITY};
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    setUpSpinner();
+
   }
 
-  public void transitionToMenuActivity(View view){
+  private void setUpSpinner(){
 
-    Intent intent = new Intent(this, MenuActivity.class);
+    Spinner spinner = findViewById(R.id.spinner);
+    ArrayAdapter adapter = new ArrayAdapter(
+            this, android.R.layout.simple_spinner_item, activities);
+    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    spinner.setAdapter(adapter);
+
+  }
+
+  public void transitionToActivity(View view){
+
+    Spinner selector = findViewById(R.id.spinner);
+    String choice = selector.getSelectedItem().toString();
+
+    goToActivity(choice);
+
+  }
+
+  private void goToActivity(String activity){
+
+    Intent intent;
+
+    switch(activity){
+
+      case MENU_ACTIVITY:
+        intent = new Intent(this, MenuActivity.class);
+        break;
+
+      default:
+        intent = new Intent(this, MenuActivity.class);
+        break;
+
+    }
+
     startActivity(intent);
 
   }
+
 }
