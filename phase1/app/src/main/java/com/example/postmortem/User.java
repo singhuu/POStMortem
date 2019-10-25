a505fe7208f8de9ed78fdaf1ebd4c2b5d99b57e5
Daniel <daniel.jarvis@mail.utoronto.ca>
2019-10-25 13:06:38 -0400

    Created the user classes fields and instance variables.

--- phase1/app/src/main/java/com/example/postmortem/User.java
+++ phase1/app/src/main/java/com/example/postmortem/User.java
@@ -1,4 +1,52 @@
 package com.example.postmortem;
 
 public class User {
+
+    private String username;
+    private String password;
+
+    private int score;
+    private int hiscore;
+
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+        this.score = 0;
+        this.hiscore = 0;
+    }
+
+    public int getScore(){
+        return this.score;
+    }
+
+    public void setScore(int score){
+        this.score = score;
+    }
+
+    public int getHiscore(){
+        return this.hiscore;
+    }
+
+    public void setHiscore(int hiscore){
+        this.hiscore = hiscore;
+    }
+
+    public String getUsername(){
+        return this.username;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public String getPassword(){
+        return this.password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+
+
 }
