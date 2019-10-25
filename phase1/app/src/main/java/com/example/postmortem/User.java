39f41725cdf7d3a38da0ec402fcb29d7e20dd9ac
Tom Howells <tomert42@gmail.com>
2019-10-25 17:34:38 -0400

    User class now contains individual game scores

--- phase1/app/src/main/java/com/example/postmortem/User.java
+++ phase1/app/src/main/java/com/example/postmortem/User.java
@@ -2,51 +2,70 @@ package com.example.postmortem;
 
 public class User {
 
-    private String username;
-    private String password;
+  private String username;
+  private String password;
 
-    private int score;
-    private int hiscore;
+  private int pickupScore;
+  private int tapScore;
+  private int typeScore;
+  private int hiScore;
 
-    public User(String username, String password){
-        this.username = username;
-        this.password = password;
-        this.score = 0;
-        this.hiscore = 0;
+  public User(String username, String password) {
+    this.username = username;
+    this.password = password;
+    this.pickupScore = 0;
+    this.tapScore = 0;
+    this.typeScore = 0;
+    this.hiScore = 0;
+  }
+
+  public int getPickupScore() {
+    return this.pickupScore;
+  }
+
+  public int getTapScore() {
+    return this.tapScore;
+  }
+
+  public int getTypeScore() {
+    return this.typeScore;
+  }
+
+  public int getHiScore() {
+    return this.hiScore;
+  }
+
+  public void setScore(int score, String game) {
+    if (game.equals("pickup")) {
+      this.pickupScore = score;
+    } else if (game.equals("tap")) {
+      this.tapScore = score;
+    } else if (game.equals("type")) {
+      this.typeScore = score;
     }
+  }
 
-    public int getScore(){
-        return this.score;
-    }
+  public int getHiscore() {
+    return this.hiScore;
+  }
 
-    public void setScore(int score){
-        this.score = score;
-    }
+  public void setHiscore(int hiscore) {
+    this.hiScore = hiscore;
+  }
 
-    public int getHiscore(){
-        return this.hiscore;
-    }
+  public String getUsername() {
+    return this.username;
+  }
 
-    public void setHiscore(int hiscore){
-        this.hiscore = hiscore;
-    }
+  public void setUsername(String username) {
+    this.username = username;
+  }
 
-    public String getUsername(){
-        return this.username;
-    }
+  public String getPassword() {
+    return this.password;
+  }
 
-    public void setUsername(String username){
-        this.username = username;
-    }
-
-    public String getPassword(){
-        return this.password;
-    }
-
-    public void setPassword(String password){
-        this.password = password;
-    }
-
-
-
+  public void setPassword(String password) {
+    this.password = password;
+  }
 }
