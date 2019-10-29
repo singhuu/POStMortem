7e881e28759fbe76add7b6e4a47efad8da2e2281
Arun Jolly Marlier <arun.jollymarlier@mail.utoronto.ca>
2019-10-28 21:02:11 -0400

    Added countdown timer and filled in subsequent onTick and onFinish methods with the latter temporarily changing back to MainActivity

--- phase1/app/src/main/java/com/example/postmortem/LevelSystems/LevelActivity.java
+++ phase1/app/src/main/java/com/example/postmortem/LevelSystems/LevelActivity.java
@@ -1,16 +1,23 @@
 package com.example.postmortem.LevelSystems;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.KeyEvent;
+
+import com.example.postmortem.MainActivity;
+import com.example.postmortem.MenuSystems.GameMenu;
+import com.example.postmortem.MenuSystems.GameOverMenu;
+import com.example.postmortem.MenuSystems.MenuActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public abstract class LevelActivity extends AppCompatActivity {
-  protected int layout;
+  protected int timeLeft;
   protected Level level;
   protected static int difficulty;
 
-  LevelActivity(int layout, Level level) {
+  /*LevelActivity(int layout, Level level) {
     this.layout = layout;
     this.level = level;
   }
@@ -20,7 +27,7 @@ public abstract class LevelActivity extends AppCompatActivity {
     setContentView(layout);
 
     this.setup();
-  }
+  }*/
 
   @Override
   public boolean onKeyDown(int keyCode, KeyEvent event)  {
@@ -36,5 +43,31 @@ public abstract class LevelActivity extends AppCompatActivity {
    * Perform initial setup of layout.
    * This includes setting the value of text boxes, buttons, etc.
    */
-  protected abstract void setup();
+  //protected abstract void setup();
+
+  /** CountDown timer that can be called by the Activity classes */
+  CountDownTimer countTimer = null;
+  /** Start timer function in SECONDS Devs reading this, Enter Time in Seconds */
+  public void startTimer(int cTimeInSeconds) {
+    countTimer =
+            new CountDownTimer(cTimeInSeconds * 1000, 1000) {
+              /** Left empty for now, I do not know what to do with this just yet */
+              public void onTick(long millisUntilFinished) {timeLeft -= 1;}
+              /** Same situation as above. Will implement as and when needed */
+              public void onFinish() {
+                switchIntent();
+              }
+            };
+    countTimer.start();
+  }
+
+  public void switchIntent(){
+    Intent intent = new Intent(this, MainActivity.class);
+    startActivity(intent);
+  }
+
+  /** cancelTimer that cancels the Count Down */
+  public void cancelTimer() {
+    if (countTimer != null) countTimer.cancel();
+  }
 }
