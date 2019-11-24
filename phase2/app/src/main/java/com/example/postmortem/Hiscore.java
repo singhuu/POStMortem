455fb25b9b0eced175cc636c0df097367037d697
Daniel <daniel.jarvis@mail.utoronto.ca>
2019-11-24 18:00:10 -0500

    Created a custom hiscore data type

---
+++ phase2/app/src/main/java/com/example/postmortem/Hiscore.java
@@ -0,0 +1,28 @@
+package com.example.postmortem;
+
+public class Hiscore {
+
+    private String username;
+    private int score;
+
+    public Hiscore(String username, int score){
+        this.score = score;
+        this.username = username;
+    }
+
+    /**
+     * Gets the value of the hiscore
+     * @return the value of the hiscore
+     */
+    public int getScore() {
+        return score;
+    }
+
+    /**
+     * Gets the username of the user who the hiscore was achieved by
+     * @return the username of the user who the hiscore was achieved by
+     */
+    public String getUsername() {
+        return username;
+    }
+}
