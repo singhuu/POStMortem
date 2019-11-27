b594b259bd093229abbd97dde37cf4d49b6bf1a9
Tom Howells <tomert42@gmail.com>
2019-11-26 20:36:14 -0500

    Finished OptionMenu layout, created OptionMenuActivity to be populated later

---
+++ phase2/app/src/main/java/com/example/postmortem/MenuSystems/OptionMenuActivity.java
@@ -0,0 +1,25 @@
+package com.example.postmortem.MenuSystems;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.postmortem.R;
+
+public class OptionMenuActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.option_menu);
+  }
+
+  public void cancel(View target) {
+    // TODO ADD FUNCTIONALITY
+  }
+
+  public void apply(View target) {
+    // TODO ADD FUNCTIONALITY
+  }
+}
