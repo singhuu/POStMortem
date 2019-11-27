b164668d37f4e5b819dcce0514fedecfcd7f087f
Tom Howells <tomert42@gmail.com>
2019-11-26 23:04:51 -0500

    MainMenuActivity partially created, need to implement start and continue game methods

--- phase2/app/src/main/java/com/example/postmortem/MenuSystems/MainMenuActivity.java
+++ phase2/app/src/main/java/com/example/postmortem/MenuSystems/MainMenuActivity.java
@@ -1,34 +1,46 @@
 package com.example.postmortem.MenuSystems;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.postmortem.GameManager;
 import com.example.postmortem.R;
 
 public class MainMenuActivity extends AppCompatActivity {
+
+  GameManager gameManager;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main_menu);
+    Bundle extras = getIntent().getExtras();
+    gameManager = (GameManager) extras.get(GameManager.INTENT_NAME);
   }
 
   public void continueGame(View target) {
-    // TODO ADD FUNCTIONALITY
+    // TODO PROPERLY IMPLEMENT CONTINUEGAME
+    gameManager.continueFromSave(this);
   }
 
   public void start(View target) {
-    // TODO ADD FUNCTIONALITY
+    // TODO PROPERLY IMPLEMENT START
+    gameManager.start(this);
   }
 
   public void openOptions(View target) {
-    // TODO ADD FUNCTIONALITY
+    Intent intent = new Intent(this, OptionMenuActivity.class);
+    intent.putExtra(GameManager.INTENT_NAME, gameManager);
+    startActivity(intent);
   }
 
   public void logout(View target) {
-    // TODO ADD FUNCTIONALITY
+    Intent intent = new Intent(this, UserSelectMenuActivity.class);
+    intent.putExtra(GameManager.INTENT_NAME, gameManager);
+    startActivity(intent);
   }
 
   /**
