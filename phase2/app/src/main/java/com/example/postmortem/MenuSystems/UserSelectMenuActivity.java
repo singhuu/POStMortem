f25a5c17f1a313effa0767bd4d9a6f45807de46f
Daniel <daniel.jarvis@mail.utoronto.ca>
2019-11-28 13:47:20 -0500

    Moved checks in user creation into new user credential validator class.

--- phase2/app/src/main/java/com/example/postmortem/MenuSystems/UserSelectMenuActivity.java
+++ phase2/app/src/main/java/com/example/postmortem/MenuSystems/UserSelectMenuActivity.java
@@ -8,7 +8,9 @@ import android.widget.EditText;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.postmortem.DataTypes.AccountException;
 import com.example.postmortem.DataTypes.User;
+import com.example.postmortem.DataTypes.UserCredentialValidator;
 import com.example.postmortem.DataTypes.UserManager;
 import com.example.postmortem.GameManager;
 import com.example.postmortem.R;
@@ -19,6 +21,7 @@ public class UserSelectMenuActivity extends AppCompatActivity {
 
   GameManager gameManager;
   UserManager userManager;
+  private UserCredentialValidator validator;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +30,7 @@ public class UserSelectMenuActivity extends AppCompatActivity {
     Bundle extras = getIntent().getExtras();
     gameManager = (GameManager) extras.get(GameManager.INTENT_NAME);
     userManager = UserManager.getManager();
+    validator = new UserCredentialValidator();
   }
 
   public void login(View target) {
@@ -34,14 +38,14 @@ public class UserSelectMenuActivity extends AppCompatActivity {
     EditText pword = (EditText) findViewById(R.id.passwordField);
 
     if (uname.getText().toString().isEmpty() | pword.getText().toString().isEmpty()) {
-      constructDialog(target, "Error","Username or password blank.");
+      constructDialog("Error","Username or password blank.");
     } else {
       // check login credentials
       Optional<User> user = userManager.attemptLogin(uname.getText().toString(), pword.getText().toString());
       if (user.isPresent()) {
         acceptLogin(user.get());
       } else {
-        constructDialog(target, "Error","Username or password incorrect.");
+        constructDialog("Error","Username or password incorrect.");
       }
     }
   }
@@ -55,22 +59,40 @@ public class UserSelectMenuActivity extends AppCompatActivity {
   }
 
   public void createUser(View target) {
-    EditText uname = (EditText) findViewById(R.id.usernameField);
-    EditText pword = (EditText) findViewById(R.id.passwordField);
+    EditText usernameField = findViewById(R.id.usernameField);
+    EditText passwordField = findViewById(R.id.passwordField);
 
-    if (uname.getText().toString().isEmpty() | pword.getText().toString().isEmpty()) {
+    String username = usernameField.getText().toString();
+    String password = passwordField.getText().toString();
+
+    tryCreateUser(username, password);
+
+    /*
+    if (usernameField.getText().toString().isEmpty() | passwordField.getText().toString().isEmpty()) {
       constructDialog(target, "Error","Username or password blank.");
     } else {
-      if (userManager.createUser(uname.getText().toString(), pword.getText().toString())) {
+      if (userManager.createUser(usernameField.getText().toString(), passwordField.getText().toString())) {
         //create user, since not already created
         constructDialog(target, "Success", "New user created.");
       } else {
         constructDialog(target, "Error","User already created.");
       }
     }
+
+     */
   }
 
-  public void constructDialog(View target, String title, String message) {
+  private void tryCreateUser(String username, String password){
+    try{
+      validator.createAccount(username, password);
+      constructDialog("success", "New user created.");
+    } catch(AccountException e){
+      String message = e.getMessage();
+      constructDialog("Error", message);
+    }
+  }
+
+  public void constructDialog(String title, String message) {
     // code modded from https://medium.com/@suragch/making-an-alertdialog-in-android-2045381e2edb
     AlertDialog.Builder builder = new AlertDialog.Builder(this);
     builder.setTitle(title);
