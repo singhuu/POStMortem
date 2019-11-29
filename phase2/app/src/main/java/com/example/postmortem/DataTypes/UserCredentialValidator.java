864206d120cf677bdac1e669ddac769f1218345e
Tom Howells <tomert42@gmail.com>
2019-11-29 14:55:36 -0500

    General cleanup of data type classes

--- phase2/app/src/main/java/com/example/postmortem/DataTypes/UserCredentialValidator.java
+++ phase2/app/src/main/java/com/example/postmortem/DataTypes/UserCredentialValidator.java
@@ -5,79 +5,79 @@ import java.util.regex.Pattern;
 
 public class UserCredentialValidator {
 
-    private UserManager manager;
+  private UserManager manager;
 
-    public UserCredentialValidator(){
-        manager = UserManager.getManager();
-    }
+  public UserCredentialValidator() {
+    manager = UserManager.getManager();
+  }
 
-    /**
-     * Gets the user matching the username and password
-     *
-     * @param username the username of the user to be logged in
-     * @param password the ppassword of the user to be logged in
-     * @return the user matching the username and password
-     * @throws AccountException whatever went wrong in the login process
-     */
-    public User login(String username, String password) throws AccountException{
-        Optional<User> user  = manager.attemptLogin(username, password);
-        if(user.isPresent()){
-            return user.get();
-        } else {
-            throw new AccountException("Username or password incorrect");
-        }
+  /**
+   * Gets the user matching the username and password
+   *
+   * @param username the username of the user to be logged in
+   * @param password the ppassword of the user to be logged in
+   * @return the user matching the username and password
+   * @throws AccountException whatever went wrong in the login process
+   */
+  public User login(String username, String password) throws AccountException {
+    Optional<User> user = manager.attemptLogin(username, password);
+    if (user.isPresent()) {
+      return user.get();
+    } else {
+      throw new AccountException("Username or password incorrect");
     }
+  }
 
-    /**
-     * Create an account with given username and password
-     * Throws an account exception if there is a problem in account creation
-     *
-     * @param username the username of the new account
-     * @param password the password of the new account
-     * @throws AccountException whatever went wrong in the creation of the account
-     */
-    public void createAccount(String username, String password) throws AccountException{
-        if(manager.checkUserExists(username)){
-            throw new AccountException("User already exists");
-        } else {
-            validateInputAndCreateAccount(username, password);
-        }
+  /**
+   * Create an account with given username and password
+   * Throws an account exception if there is a problem in account creation
+   *
+   * @param username the username of the new account
+   * @param password the password of the new account
+   * @throws AccountException whatever went wrong in the creation of the account
+   */
+  public void createAccount(String username, String password) throws AccountException {
+    if (manager.checkUserExists(username)) {
+      throw new AccountException("User already exists");
+    } else {
+      validateInputAndCreateAccount(username, password);
     }
+  }
 
-    /**
-     * validates the input and creates the new user in the manager
-     *
-     * @param username the username of the new account
-     * @param password the password of the new account
-     * @throws AccountException whatever went wrong in the creation of the account
-     */
-    private void validateInputAndCreateAccount(String username, String password) throws AccountException{
-        if(validInput(username, password)){
-            createAccountInManager(username, password);
-        } else {
-            throw new AccountException("Username or password must be nonempty string with no \',\''s.");
-        }
+  /**
+   * validates the input and creates the new user in the manager
+   *
+   * @param username the username of the new account
+   * @param password the password of the new account
+   * @throws AccountException whatever went wrong in the creation of the account
+   */
+  private void validateInputAndCreateAccount(String username, String password) throws AccountException {
+    if (validInput(username, password)) {
+      createAccountInManager(username, password);
+    } else {
+      throw new AccountException("Username or password must be nonempty string with no \',\''s.");
     }
+  }
 
-    /**
-     * Checks the inputs against a pattern of valid inputs
-     *
-     * @param username the username of the new account
-     * @param password the password of the new account
-     * @return if the username and password match the pattern of valid inputs
-     */
-    private boolean validInput(String username, String password){
-        String pattern = "[^,]+";
-        return Pattern.matches(pattern, username) && Pattern.matches(pattern, password);
-    }
+  /**
+   * Checks the inputs against a pattern of valid inputs
+   *
+   * @param username the username of the new account
+   * @param password the password of the new account
+   * @return if the username and password match the pattern of valid inputs
+   */
+  private boolean validInput(String username, String password) {
+    String pattern = "[^,]+";
+    return Pattern.matches(pattern, username) && Pattern.matches(pattern, password);
+  }
 
-    /**
-     * Creates the new account in the manager
-     *
-     * @param username the username of the new account
-     * @param password the password of the new account
-     */
-    private void createAccountInManager(String username, String password){
-        manager.createUser(username, password);
-    }
+  /**
+   * Creates the new account in the manager
+   *
+   * @param username the username of the new account
+   * @param password the password of the new account
+   */
+  private void createAccountInManager(String username, String password) {
+    manager.createUser(username, password);
+  }
 }
--- phase2/app/src/main/java/com/example/postmortem/DataTypes/UserManager.java
+++ phase2/app/src/main/java/com/example/postmortem/DataTypes/UserManager.java
@@ -1,6 +1,5 @@
 package com.example.postmortem.DataTypes;
 
-import com.example.postmortem.DataTypes.User;
 import com.example.postmortem.FileInterfacing.UserFileController;
 
 import java.util.List;
@@ -8,104 +7,103 @@ import java.util.Optional;
 
 public class UserManager {
 
-    private static UserManager manager;
+  private static UserManager manager;
 
-    private UserFileController fileController;
-    private List<User> users;
+  private UserFileController fileController;
+  private List<User> users;
 
 
+  private UserManager(String appDataDir) {
+    this.fileController = new UserFileController(appDataDir);
+    this.refresh();
+  }
 
-    private UserManager(String appDataDir){
-        this.fileController = new UserFileController(appDataDir);
-        this.refresh();
+  /**
+   * Sets the directory for the application data and creates the instance of the class
+   *
+   * @param directory the directory of the program files
+   */
+  public static void initialize(String directory) {
+    manager = new UserManager(directory);
+  }
+
+  /**
+   * Gets the manager for users
+   * There is only one instance of this class at a time, which can be accessed by many
+   * classes at once
+   *
+   * @return the user manger
+   */
+  public static UserManager getManager() {
+    return manager;
+  }
+
+  public List<User> getUsers() {
+    return users;
+  }
+
+
+  private void refresh() {
+    this.users = (List<User>) fileController.load();
+  }
+
+  public void saveState() {
+    this.fileController.save(users);
+  }
+
+  /**
+   * Try to log into an user account
+   *
+   * @param username the username of the desired account
+   * @param password the password of the desired account
+   * @return optionally the user logged in to
+   */
+  private Optional<User> attemptLogin(String username, String password) {
+    Optional user = Optional.empty();
+    for (User account : users) {
+      if (loginCorrect(account, username, password)) {
+        user = Optional.of(account);
+        break;
+      }
     }
 
-    /**
-     * Sets the directory for the application data and creates the instance of the class
-     *
-     * @param directory the directory of the program files
-     */
-    public static void initialize(String directory){
-        manager = new UserManager(directory);
+    return user;
+  }
+
+  private boolean loginCorrect(User user, String username, String password) {
+    return user.getUsername().equals(username) && user.getPassword().equals(password);
+  }
+
+  /**
+   * Create a new user and add it to the users list
+   *
+   * @param username the new accounts username
+   * @param password the new accounts password
+   * @return if the account was successfully created
+   */
+  boolean createUser(String username, String password) {
+    boolean canCreate = !checkUserExists(username);
+    if (canCreate) {
+      users.add(new User(username, password));
     }
+    return canCreate;
+  }
 
-    /**
-     * Gets the manager for users
-     * There is only one instance of this class at a time, which can be accessed by many
-     * classes at once
-     *
-     * @return the user manger
-     */
-    public static UserManager getManager(){
-        return manager;
+  boolean checkUserExists(String username) {
+    for (User user : users) {
+      if (user.getUsername().equals(username)) {
+        return true;
+      }
     }
+    return false;
+  }
 
-    public List<User> getUsers(){
-        return users;
-    }
-
-
-
-    public void refresh(){
-        this.users = (List<User>) fileController.load();
-    }
-
-    public void saveState(){
-        this.fileController.save(users);
-        //refresh();
-    }
-
-    /**
-     * Try to log into an user account
-     * @param username the username of the desired account
-     * @param password the password of the desired account
-     * @return optionally the user logged in to
-     */
-    public Optional<User> attemptLogin(String username, String password){
-        Optional user = Optional.empty();
-        for (User account : users) {
-            if(loginCorrect(account, username, password)){
-                user = Optional.of(account);
-                break;
-            }
-        }
-
+  public User getUser(String username) {
+    for (User user : users) {
+      if (user.getUsername().equals(username)) {
         return user;
+      }
     }
-
-    private boolean loginCorrect(User user, String username, String password){
-        return user.getUsername().equals(username) && user.getPassword().equals(password);
-    }
-
-    /**
-     * Create a new user and add it to the users list
-     * @param username the new accounts username
-     * @param password the new accounts password
-     * @return if the account was successfully created
-     */
-    public boolean createUser(String username, String password){
-        boolean canCreate = !checkUserExists(username);
-        if(canCreate){
-            users.add(new User(username, password));
-        }
-        return canCreate;
-    }
-
-    public boolean checkUserExists(String username){
-        for(User user: users){
-            if(user.getUsername().equals(username)){
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public User getUser(String username){
-        for (User user: users) {
-            if(user.getUsername().equals(username)){
-                return user;
-            }
-        }
-        return null;
-    }
+    return null;
+  }
 }
